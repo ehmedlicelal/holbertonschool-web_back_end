bfabcf5d10aba199af1e0bf213137372ca053282
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-05-24 23:33:16 +0400

    Create 4-define_variables.py with variable annotations
    Added example variable annotations for basic types.

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Example variable annotations for basic built-in types."""
+
+# Integer variable annotated explicitly.
+a: int = 1
+
+# Float variable annotated explicitly.
+pi: float = 3.14
+
+# Boolean variable annotated explicitly.
+i_understand_annotations: bool = True
+
+# String variable annotated explicitly.
+school: str = "Holberton"
