a43d348001ac18f03f42990ff895b69556c38b87
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-05-24 23:51:54 +0400

    Refactor to_kv function to return squared value

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
+"""Module that defines to_kv function."""
+from typing import Tuple, Union
 
-"""Module that defines a list of floats and integers."""
-from typing import List, Union, Tuple
 
-
-def to_kv(k: str, v: Union[int, float]) -> tuple:
-    user_data: Tuple[int, Union[int, float]] = (k, v)
-    return user_data
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    return (k, float(v ** 2))
