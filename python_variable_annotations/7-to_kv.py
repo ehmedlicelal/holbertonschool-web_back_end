0a710071e9ab358ec55a4c35754eb8981174e719
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-05-24 23:53:23 +0400

    Add docstring to to_kv function
    Add docstring to explain the function's return value.

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,4 +5,5 @@ from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Returns a tuple with the string k and the square of v as a float."""
     return (k, float(v ** 2))
